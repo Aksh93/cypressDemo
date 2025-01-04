e42e12957682c251a288e286215328ee7a7a5dcd
Akshay Patil <155887780+Aksh93@users.noreply.github.com>
2025-01-04 12:47:27 +0530

    Update login.cy.js

--- cypress/e2e/phoenix/login.cy.js
+++ cypress/e2e/phoenix/login.cy.js
@@ -9,7 +9,7 @@ describe("login feature", function() {
 
         cy.visit("http://phoenix.techwithjatin.com");
         cy.get("[class='mat-focus-indicator fuse-mat-button-large w-full mt-6 ng-tns-c135-0 mat-flat-button mat-button-base mat-primary']").
-        should("have.text", " Sign inn ");
+        should("have.text", " Sign in ");
 
     })
 
