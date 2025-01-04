bbdd502d9cae4b65acc11ad3217b4df736c50297
Aksh93 <akshaykumarpatil84@gmail.com>
2025-01-04 18:36:22 +0530

    NEW CHANGES

--- cypress/e2e/phoenix/login.cy.js
+++ cypress/e2e/phoenix/login.cy.js
@@ -10,7 +10,7 @@ describe("login feature", function() {
         cy.visit("http://phoenix.techwithjatin.com");
         cy.get("[class='mat-focus-indicator fuse-mat-button-large w-full mt-6 ng-tns-c135-0 mat-flat-button mat-button-base mat-primary']").
         should("have.text", " Sign in ");
-
+///AKSHAY NEW 
     })
 
 })
